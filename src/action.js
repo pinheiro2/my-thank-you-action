3832be82bc88d4a33d4bab58706c566db891e644
pinheiro2 <tiagopsilva4775@gmail.com>
2025-11-04 14:44:37 +0000

    Adição de um GIF

--- src/action.js
+++ src/action.js
@@ -1,9 +1,17 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fetch = require('node-fetch');
 
 async function run() {
+    const TENOR_TOKEN = core.getInput('TENOR_TOKEN');
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
     const octokit = github.getOctokit(GITHUB_TOKEN);
+
+    const randomPos = Math.round(Math.random() * 1000);
+    const url = `https://api.tenor.com/v1/search?q=thank%20you&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
+    const response = await fetch(url);
+    const { results } = await response.json();
+    const gifUrl = results[0].media[0].tinygif.url;
 
     const { context = {} } = github;
     const { pull_request } = context.payload;
@@ -11,7 +19,7 @@ async function run() {
     await octokit.rest.issues.createComment({
       ...context.repo,
       issue_number: pull_request.number,
-      body: 'Thank you for submitting a pull request! We will try to review this as soon as we can.',
+      body: `Thank you for submitting a pull request! We will try to review this as soon as we can.\n\n<img src="${gifUrl}" alt="thank you" />`,
     });
 
 }
